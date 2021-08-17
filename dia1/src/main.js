7abaee9abb40012fadfeff1b5efea34c3664d7d3
vbertazzo <vbertazzo@gmail.com>
2021-08-17 10:01:49 -0300

    Complete exercise 4

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,17 @@
 import './style.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
-  <h1>B. Academy</h1>
-  <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
+const app = document.querySelector('[data-js="app"]')
+app.innerHTML = `
+<h1>B. Academy</h1>
+<p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+const visibilityButton = document.querySelector('[data-js="toggle-visibility"')
+
+const toggleContentVisibility = () => {
+  app.style.display = app.style.display === 'none' ? 'block' : 'none'
+  app.style.display === 'none'
+    ? (visibilityButton.textContent = 'Mostrar conteúdo')
+    : (visibilityButton.textContent = 'Esconder conteúdo')
+}
+
+visibilityButton.addEventListener('click', toggleContentVisibility, false)
