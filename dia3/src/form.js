1c82bb86cb0bd84456f7ed7c91cd9ad888e9f668
vbertazzo <vbertazzo@gmail.com>
2021-08-19 10:52:47 -0300

    Complete day 3 exercise 2

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,4 +1,7 @@
+import './style.css'
+
 const userName = document.querySelector('[data-js="name"]')
+const form = document.querySelector('[data-js="form"')
 
 const shouldCapitalizeWord = word => {
   const exceptions = ['de', 'da', 'do', 'dos']
@@ -16,3 +19,52 @@ userName.addEventListener('input', e => {
   const words = e.target.value.split(' ')
   e.target.value = words.map(word => capitalizeWord(word)).join(' ')
 })
+
+const createColorContainer = colors => {
+  const colorElements = colors.map(color => {
+    const square = document.createElement('div')
+    square.className = 'square'
+    square.style.backgroundColor = color
+
+    return square
+  })
+
+  const colorContainer = document.createElement('div')
+
+  for (const element of colorElements) {
+    colorContainer.insertAdjacentElement('beforeend', element)
+  }
+
+  return colorContainer
+}
+
+const createColorSelect = colors => {
+  const colorSelect = document.createElement('select')
+  colorSelect.setAttribute('data-js', 'select')
+  colorSelect.setAttribute('multiple', '')
+
+  for (const color of colors) {
+    const option = document.createElement('option')
+    option.setAttribute('value', color)
+    option.textContent = color
+
+    colorSelect.insertAdjacentElement('beforeend', option)
+  }
+
+  colorSelect.addEventListener('change', e => {
+    const newColorContainer = createColorContainer(
+      [...e.target.selectedOptions].map(option => option.value)
+    )
+    colorSection.innerHTML = ''
+    colorSection.insertAdjacentElement('beforeend', newColorContainer)
+  })
+
+  return colorSelect
+}
+
+const COLORS = ['red', 'yellow', 'green', 'blue', 'violet']
+const colorSelect = createColorSelect(COLORS)
+form.insertAdjacentElement('beforeend', colorSelect)
+
+const colorSection = document.createElement('section')
+form.insertAdjacentElement('beforeend', colorSection)
