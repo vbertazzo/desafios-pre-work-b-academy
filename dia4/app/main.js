a6b20e675c5b1a5f9c1d083caa6ee93b60b359c1
vbertazzo <vbertazzo@gmail.com>
2021-08-20 12:03:42 -0300

    Complete day 4 exercise 3

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,6 +1,9 @@
 import './style.css'
 
+const form = document.querySelector('[data-js="cars-form"]')
 const carsTable = document.querySelector('[data-js="table"]')
+
+const URL = 'http://localhost:3333/cars'
 
 const elementTypes = {
   image: createImage,
@@ -33,12 +36,6 @@ function createColor (value) {
   return td
 }
 
-function addMessageToTable (message) {
-  const messageElement = document.createElement('p')
-  messageElement.textContent = message
-  carsTable.appendChild(messageElement)
-}
-
 function createCarRow (car) {
   const elements = [
     { type: 'image', value: car.image },
@@ -57,6 +54,27 @@ function createCarRow (car) {
   return tr
 }
 
+function showErrorMessage (content) {
+  const message = document.createElement('p')
+  message.textContent = content
+  message.setAttribute('data-js', 'message')
+  message.style.color = 'red'
+  form.insertAdjacentElement('afterend', message)
+}
+
+function hideErrorMessage () {
+  const message = document.querySelector('[data-js="message"]')
+  if (message) {
+    message.remove()
+  }
+}
+
+function addMessageToTable (message) {
+  const messageElement = document.createElement('p')
+  messageElement.textContent = message
+  carsTable.appendChild(messageElement)
+}
+
 function addCarsToTable (cars) {
   const carElements = document.createDocumentFragment()
 
@@ -68,13 +86,85 @@ function addCarsToTable (cars) {
   carsTable.appendChild(carElements)
 }
 
-fetch('http://localhost:3333/cars')
-  .then(result => result.ok && result.json())
-  .then(cars => {
-    cars.length === 0
-      ? addMessageToTable('Nenhum carro encontrado')
-      : addCarsToTable(cars)
-  })
-  .catch(error => {
-    console.error('Fetch error: ', error)
-  })
+async function addCarToApi (car) {
+  try {
+    const response = await fetch(URL, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(car)
+    })
+    const data = await response.json()
+
+    return data
+  } catch (error) {
+    console.log('Erro ao adicionar carro:', error)
+    showErrorMessage(
+      'Erro ao adicionar carro. Por favor, tente novamente em instantes.'
+    )
+
+    return
+  }
+}
+
+async function getCarsFromApi () {
+  try {
+    const response = await fetch(URL)
+    const data = await response.json()
+
+    return data
+  } catch (error) {
+    console.log('Erro ao carregar carros:', error)
+    showErrorMessage('Erro ao carregar carros do servidor.')
+
+    return
+  }
+}
+
+async function render () {
+  const cars = await getCarsFromApi()
+
+  if (!cars) {
+    return
+  }
+
+  if (cars.length === 0) {
+    addMessageToTable('Nenhum carro encontrado')
+
+    return
+  }
+
+  carsTable.innerHTML = ''
+  addCarsToTable(cars)
+}
+
+const getFormElement = event => name => event.target.elements[name]
+
+form.addEventListener('submit', async e => {
+  e.preventDefault()
+  hideErrorMessage()
+
+  const getElement = getFormElement(e)
+  const newCar = {
+    image: getElement('image').value,
+    brandModel: getElement('brand-model').value,
+    year: getElement('year').value,
+    plate: getElement('plate').value,
+    color: getElement('color').value
+  }
+
+  const response = await addCarToApi(newCar)
+
+  if (response.error) {
+    showErrorMessage(response.message)
+    return
+  }
+
+  await render()
+
+  e.target.reset()
+  getElement('image').focus()
+})
+
+await render()
